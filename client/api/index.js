3ac8ba80fe0ef310bcda31d5a0b9505a9b6218fa
S. Manongga <smanongga@gmail.com>
2017-06-02 10:25:01 +1200

    AddBit Form

--- client/api/index.js
+++ client/api/index.js
@@ -1,14 +1,13 @@
 import request from 'superagent'
 
-export function apiGetAllListings(cb){
-    request.get('/listings')
-    end((err, res) => {
-      if (err) {
-        cb(err.message)
-        return
-      }
-      const result = res.body
-      cb(null, result)
-    })
+export function apiGetAllListings (cb) {
+  request.get('/listings')
+  end((err, res) => {
+    if (err) {
+      cb(err.message)
+      return
+    }
+    const result = res.body
+    cb(null, result)
+  })
 }
-
--- client/components/ViewItemAndBid.jsx
+++ client/components/ViewItemAndBid.jsx
@@ -1,34 +1,34 @@
 import React from 'react'
 
-import bidItems from '../../data/beers'
+import bidItems from '../../server/data'
 
 const ViewItemAndBid = (props) => {
   return (
     <div className='row view-item-bid'>
     <div className='col-md-12 item-title'>{props.name}</div>
-    <div className='col-md-4 item-image'><img src={props.pictureUrl} /></div>
-    <div className='cold-md-8 item-description'><p>{props.description}</p></div>
+    <div className='col-md-4'>
+      <div className='image'>
+      <img src={props.picture_url} />
     </div>
+    <div className='col-md-8'>
+      <div className='submitted-by'><p><b>Submitted By:</b><br />{props.user_name}</p></div>
+      <div className='description'><p><b>Description:</b><br />{props.description}</p></div>
+      <div className='starting-bid'><p><b>Starting Bid:</b><br />{props.starting_bid}</p></div>
+      <div className='row bid-listing'>
+        {props.bids.map(bid => {
+              return (
+                <div key={bid.id}>
+                  <p>{bid.user_name}</p>
+                  {bid.bid_date}
+                  {bid.bid_amount}
+
+                </div>
+              )
+            })}
+      </div>
+    </div>
+
   )
 }
 
-function getItemFromId (id) {
-  const item = beerData.beers.find(beer => beer.id === id)
-  return beer.name
-}
-
 export default ViewItemAndBid
-
-
-
-// {props.cart.map((item, id) => {
-//   const name = getNameFromId(item.id)
-//   return (
-//     <tr key={id}>
-//       <td>{name}</td>
-//       <td><input className='update-input' value={item.quantity} /></td>
-//       <td><button><span className='fa fa-trash fa-2x' /></button></td>
-//       {/* TODO: implement deletes */}
-//     </tr>
-//   )
-// })}
