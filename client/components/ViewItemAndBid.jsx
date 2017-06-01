85d33f4670cb29cca23e5e7189381d1b8bf18a4a
S. Manongga <smanongga@gmail.com>
2017-06-02 11:10:10 +1200

    Change to this.state

--- client/components/ViewItemAndBid.jsx
+++ client/components/ViewItemAndBid.jsx
@@ -1,34 +1,55 @@
 import React from 'react'
+// import AddBid from './AddBid'
 
-import bidItems from '../../server/data'
 
-const ViewItemAndBid = (props) => {
-  return (
-    <div className='row view-item-bid'>
-    <div className='col-md-12 item-title'>{props.name}</div>
-    <div className='col-md-4'>
-      <div className='image'>
-      <img src={props.picture_url} />
-    </div>
-    <div className='col-md-8'>
-      <div className='submitted-by'><p><b>Submitted By:</b><br />{props.user_name}</p></div>
-      <div className='description'><p><b>Description:</b><br />{props.description}</p></div>
-      <div className='starting-bid'><p><b>Starting Bid:</b><br />{props.starting_bid}</p></div>
-      <div className='row bid-listing'>
-        {props.bids.map(bid => {
-              return (
-                <div key={bid.id}>
-                  <p>{bid.user_name}</p>
-                  {bid.bid_date}
-                  {bid.bid_amount}
+export default class ViewItemAndBid extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      listing: { name: '', description: '', starting_bid: '', user_name: '', picture_url: '', bids: [] }
+    }
+  }
 
-                </div>
-              )
-            })}
+  render () {
+    return (
+      <div className='row view-item-bid'>
+        <div className='col-md-12 item-title'>{this.state.listing.name}</div>
+        <div className='col-md-4'>
+          <div className='image'>
+            <img src={this.state.listing.picture_url} />
+          </div>
+        </div>
+        <div className='col-md-8'>
+          <div className='submitted-by'><p><b>Submitted By:</b><br />{this.state.listing.user_name}</p></div>
+          <div className='description'><p><b>Description:</b><br />{this.state.listing.description}</p></div>
+          <div className='starting-bid'><p><b>Starting Bid:</b><br />{this.state.listing.starting_bid}</p></div>
+          <div className='row bid-listing'>
+            <table>
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Date</th>
+                  <th>Amount</th>
+                </tr>
+              </thead>
+              <tbody>
+              {this.state.listing.bids.map(bid => {
+                return (
+                  <tr>
+                  <td key={bid.id}>{bid.user_name}</td>
+                  <td>{bid.bid_date}</td>
+                  <td>{bid.bid_amount}</td>
+                </tr>
+                )
+              })}
+            </tbody>
+          </table>
+        </div>
+        <div className='bid-form'>
+          {/* <AddBid bidItem={props.listing.id} /> */}
+        </div>
       </div>
     </div>
-
   )
 }
-
-export default ViewItemAndBid
+}
